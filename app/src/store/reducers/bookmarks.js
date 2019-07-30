249947f35247aa42065ff85212409106a5b51e45
quangChe <quang.n.che@gmail.com>
2019-07-30 15:22:32 -0700

    Redux
    Installed Redux and added some 'bookmarks' reducers and action types
    that the app will benefit from.

---
+++ app/src/store/reducers/bookmarks.js
@@ -0,0 +1,13 @@
+const initialState = {
+  bookmarks: [],
+  currentlyViewing: null
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state;
+  }
+};
+
+export default reducer;
