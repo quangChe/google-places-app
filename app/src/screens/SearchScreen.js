e5528a80d73818725cef675199d5ddb66f7af27f
quangChe <quang.n.che@gmail.com>
2019-07-30 14:35:24 -0700

    SearchScreen Completed
    Completed all functionality and rendering of SearchScreen. Removed
    SearchModal for good.

--- app/src/screens/SearchScreen.js
+++ app/src/screens/SearchScreen.js
@@ -14,6 +14,7 @@ import axios from 'axios';
 import { API_KEY } from '../../API_KEY';
 
 import SearchInput from '../components/SearchInput';
+import SearchResults from '../components/SearchResults';
 
 export default class SearchScreen extends Component {
   state = {
@@ -21,10 +22,49 @@ export default class SearchScreen extends Component {
     displayAttribution: true,
   };
 
+  autoComplete = async (val) => {
+    const googRes = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${API_KEY}&input=${val}`);
+    const places = googRes.data.predictions.map(place => {
+      return { 
+        key: place.place_id,
+        description: place.description 
+      };
+    });
+    this.setState({results: places, displayAttribution: false});
+  }
+
+  findPlace = async (event) => {
+    const val = event.nativeEvent.text;
+    const googRes = await axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${API_KEY}&inputtype=textquery&input=${val}`);
+    this.setState({displayAttribution: false});
+    this.getPlaceDetails(googRes.data.candidates[0].place_id);
+  }
+
+  getPlaceDetails = async (id) => {
+    const place = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?key=${API_KEY}&placeid=${id}`); 
+    
+    // TODO: Use Redux to store currently viewing place
+    console.log(place.data.result);
+    this.props.navigation.navigate('Place');
+  }
+
+  cancelSearch = () => {
+    this.props.navigation.goBack();
+  }
+
   render() {
+    const { results, displayAttribution } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>TESTING SEARCH!</Text>
+        <SearchInput 
+          onChange={this.autoComplete} 
+          onSubmit={this.findPlace}
+          onCancel={this.cancelSearch}/>
+        <SearchResults
+          displayAttribution={displayAttribution} 
+          results={results} 
+          onClick={this.getPlaceDetails}/>
       </View>
     )
   }
@@ -33,6 +73,5 @@ export default class SearchScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
   }
 })
