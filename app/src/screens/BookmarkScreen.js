b9d23bdb52dc3d8695ffd1ec46fcfdca01f5463d
quangChe <quang.n.che@gmail.com>
2019-07-30 17:03:32 -0700

    Cleaned/converted BookmarkScreen

--- app/src/screens/BookmarkScreen.js
+++ app/src/screens/BookmarkScreen.js
@@ -1,37 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { 
   View,
   StyleSheet,
 } from 'react-native';
 
-import BookmarkCardImg from '../../assets/image/bookmarkCard.png';
-
 import Header from '../components/BookmarkHeader';
 import BookmarkSlide from '../components/BookmarkSlide/BookmarkSlide';
 import Footer from '../components/BookmarkFooter';
 
-export default class BookmarkScreen extends Component {
-  state = {
-    bookmarks: [
-      {key: '1', image: BookmarkCardImg, details: { name: 'Royals Hot Chicken', rating: 4.3, location: 'Louisville, Kentucky'}},
-      {key: '2', image: BookmarkCardImg, details: { name: 'Royals Hot Chicken', rating: 4.3, location: 'Louisville, Kentucky'}},
-      {key: '3', image: BookmarkCardImg, details: { name: 'Royals Hot Chicken', rating: 4.3, location: 'Louisville, Kentucky'}},
-      {key: '4', image: BookmarkCardImg, details: { name: 'Royals Hot Chicken', rating: 4.3, location: 'Louisville, Kentucky'}},
-      {key: '5', image: BookmarkCardImg, details: { name: 'Royals Hot Chicken', rating: 4.3, location: 'Louisville, Kentucky'}},
-      {key: '6', image: BookmarkCardImg, details: { name: 'Royals Hot Chicken', rating: 4.3, location: 'Louisville, Kentucky'}}
-    ],
-  };
-
-  render() {
-    const navigation = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <Header navigation={navigation} pressBookmark={this.search}/>
-        <BookmarkSlide bookmarks={this.state.bookmarks}/>
-        <Footer/>
-      </View>
-    );
-  };
+const BookmarkScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <BookmarkSlide/>
+      <Footer/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -41,3 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
 });
+
+export default BookmarkScreen;
